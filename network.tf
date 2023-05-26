98f3dd266da41d3a36fb5293e5657c54d1cf36a2
hashi-shaohern <100564490+hashi-shaohern@users.noreply.github.com>
2023-05-26 12:57:31 +0800

    Update network.tf 3

--- network.tf
+++ network.tf
@@ -16,11 +16,5 @@ module "network" {
     "subnet2" : ["Microsoft.Sql"],
     "subnet3" : ["Microsoft.Sql"]
   }
-  use_for_each = true
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-  }
 
-  depends_on = [azurerm_resource_group.example]
 }
