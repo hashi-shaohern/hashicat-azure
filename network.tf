54f6c00dbd245cf176e40245b6b95937237cf299
hashi-shaohern <100564490+hashi-shaohern@users.noreply.github.com>
2023-05-26 13:03:27 +0800

    Update network.tf 7

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "example" {
   name     = "my-resources"
   location = "West Europe"
 }
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "test" {
 module "network" {
   source  = "app.terraform.io/shaohern-training/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "test"
+  resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -17,4 +17,10 @@ module "network" {
     "subnet3" : ["Microsoft.Sql"]
   }
 
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.example]
 }
