ce2d7946779cdcdf9522e4300ae67a9d0ebe372a
hashi-shaohern <100564490+hashi-shaohern@users.noreply.github.com>
2023-05-26 12:51:45 +0800

    Update network.tf

--- network.tf
+++ network.tf
@@ -8,13 +8,12 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "network" {
-  source  = "app.terraform.io/shaohern-training/network/azurerm"
-  version = "5.2.0"
+  source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-  
+
   subnet_service_endpoints = {
     "subnet1" : ["Microsoft.Sql"],
     "subnet2" : ["Microsoft.Sql"],
@@ -24,4 +23,7 @@ module "network" {
   tags = {
     environment = "dev"
     costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.example]
 }
